06844143cbdd0b15fb8cadfa69df80f6ded2f9c1
Oleg Veremei <veremeioleg@gmail.com>
2023-12-08 08:55:27 +0300

    Refactor BaseTest.java

--- src/test/java/by/veremei/tests/BaseTest.java
+++ src/test/java/by/veremei/tests/BaseTest.java
@@ -21,7 +21,7 @@ public class BaseTest {
         Configuration.browserSize = System.getProperty(" browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty(" browserVersion", "100.0");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid", "selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteURL", "selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
